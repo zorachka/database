3ff0ec8072a3399fd8b6c43ee5a5c70361f7876d
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-12-25 14:16:46 +0400

    refactor: DatabaseServiceProvider

--- src/Database/DatabaseServiceProvider.php
+++ src/Database/DatabaseServiceProvider.php
@@ -4,16 +4,7 @@ declare(strict_types=1);
 
 namespace Zorachka\Framework\Database;
 
-use Zorachka\Framework\Console\ConsoleConfig;
 use Zorachka\Framework\Container\ServiceProvider;
-use Zorachka\Framework\Database\Cycle\Migrations\Console\Migration\CreateCommand;
-use Zorachka\Framework\Database\Cycle\Migrations\Console\Migration\DownCommand;
-use Zorachka\Framework\Database\Cycle\Migrations\Console\Migration\ListCommand;
-use Zorachka\Framework\Database\Cycle\Migrations\Console\Migration\UpCommand;
-use Zorachka\Framework\Database\Cycle\Migrations\Event\AfterMigrate;
-use Zorachka\Framework\Database\Cycle\Migrations\Event\BeforeMigrate;
-use Zorachka\Framework\EventDispatcher\EventDispatcherConfig;
-use Zorachka\Framework\EventDispatcher\NullableEventListener;
 
 final class DatabaseServiceProvider implements ServiceProvider
 {
@@ -24,7 +15,6 @@ final class DatabaseServiceProvider implements ServiceProvider
     {
         return [
             DatabaseConfig::class => fn() => DatabaseConfig::withDefaults(),
-            MigrationsConfig::class => fn() => MigrationsConfig::withDefaults(),
         ];
     }
 
@@ -33,14 +23,6 @@ final class DatabaseServiceProvider implements ServiceProvider
      */
     public static function getExtensions(): array
     {
-        return [
-            ConsoleConfig::class => function(ConsoleConfig $config) {
-                return $config
-                    ->withCommand(ListCommand::class)
-                    ->withCommand(CreateCommand::class)
-                    ->withCommand(UpCommand::class)
-                    ->withCommand(DownCommand::class);
-            },
-        ];
+        return [];
     }
 }
